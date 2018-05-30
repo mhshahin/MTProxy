8f30e4993216367d318e11173d8f6b1349da4bee
Mohammad H. Shahin <mhshahin91@gmail.com>
2018-05-30 18:18:51 +0430

    Delete start.sh

--- start.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-
-# Use "tail -f /dev/null" to run the process in foreground
-# in order to prevent the container to stop immediately
-# after starting the container
-pm2 start mtproxy.js -i max | tail -f /dev/null
