7170208b96139b0b0b6ad41046942a1cbd4564d0
mhshahin <mhshahin91@gmail.com>
2018-05-31 02:44:24 +0430

    setup.sh

--- setup.sh
+++ setup.sh
@@ -1,16 +1,28 @@
 #! /bin/bash
 
+# Change values to your desired values
+YOUR_PORT=9500
+YOUR_SECRET=b0cbcef5a486d9636472ac27f8e12a8c
+
+sed -i "s/b0cbcef5a486d9636472ac27f8e12a8c/${YOUR_SECRET}/" Dockerfile
+sed -i "s/9500/${YOUR_PORT}/" Dockerfile
+
 echo "Building mtproxy docker image..."
 
 sleep 3
 
 docker build -t mtproxy .
 
+echo "Running mtproxy image ..."
+
+sleep 3
+
+docker run -itd --rm -p ${YOUR_PORT}:${YOUR_PORT} mtproxy
+
 MTPROXY_ID=$(docker ps | grep mtproxy | awk '{print $1}')
 
-echo ${MTPROXY_ID}
 
-echo "Running mtproxy image ..."
+echo "Running mtproxy service ..."
 
 sleep 3
 
